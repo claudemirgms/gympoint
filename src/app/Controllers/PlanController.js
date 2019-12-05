7b0b82922f0ce4159c178fe9545347a401588bb7
Claudemir Gomes dos Santos <claudemirgms@gmail.com>
2019-12-04 22:28:13 -0300

    bootcamp-gostack-desafio-03/gestao de planos

---
+++ src/app/Controllers/PlanController.js
@@ -0,0 +1,50 @@
+import Plan from '../models/Plan';
+
+class PlanController {
+  async store(req, res) {
+    const planExists = await Plan.findOne({
+      where: {
+        title: req.body.title,
+      },
+    });
+
+    if (planExists) {
+      return res.status(400).json({ error: 'Plan already exists' });
+    }
+
+    const plan = await Plan.create(req.body);
+
+    return res.json(plan);
+  }
+
+  async index(req, res) {
+    const plan = await Plan.findAll();
+    return res.json(plan);
+  }
+
+  async update(req, res) {
+    const plan = await Plan.findByPk(req.params.id);
+
+    if (!plan) {
+      return res.status(400).json({ error: 'plan not found' });
+    }
+
+    await plan.update(req.body);
+
+    return res.json(plan);
+  }
+
+  async delete(req, res) {
+    const plan = await Plan.findByPk(req.params.id);
+
+    if (!plan) {
+      return res.status(400).json({ error: 'plan not found' });
+    }
+
+    await plan.delete();
+
+    return res.status(200);
+  }
+}
+
+export default new PlanController();
--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,20 @@ import { Router } from 'express';
 
 import SessionController from './app/Controllers/SessionController';
 import StudentsController from './app/Controllers/StudentsController';
+import PlanController from './app/Controllers/PlanController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/session', SessionController.store);
+
 routes.use(authMiddleware);
 routes.post('/student', StudentsController.store);
+
+routes.post('/plan', PlanController.store);
+routes.get('/plans', PlanController.index);
+routes.put('/plan/:id', PlanController.update);
+routes.delete('/deleteplan/:id', PlanController.delete);
 
 export default routes;
